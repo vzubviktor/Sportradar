747b7ec1c515292378667749795261fad8c5c452
Ekaterina <kapega@yandex.ru>
2021-09-28 05:32:15 -0400

    added more tests

--- src/tests/Api.test.js
+++ src/tests/Api.test.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {fetchTournaments, fetchMatches, dateConstructor} from "../components/Api";
+import {fetchTournaments, fetchMatches, dateConstructor, sortMatches} from "../components/Api";
 
 
  // testing fetchTournaments() to receive an array of objects
@@ -8,7 +8,7 @@ import {fetchTournaments, fetchMatches, dateConstructor} from "../components/Api
 it('fetches successfully array from API', async () => {
     const result = await fetchTournaments()
     .then((res) => expect(res instanceof Array).toBe(true))
-  })
+  });
  
 // testing fetchTournaments() to receive Non empty of objects
  // from source API that conatin tournament objects
@@ -16,7 +16,7 @@ it('fetches successfully array from API', async () => {
 it('check if fetched array non empty', async () => {
     const result = await fetchTournaments()
     .then((res) => expect(res.length !== 0).toBe(true))
-})
+});
 
 // teisting fetchMatches function. Test is passed if we receieve correct
 // ID of tournament, check if its number, fetches matches corresponding to tournament, 
@@ -35,7 +35,9 @@ it('fetches matches from tournamentID and check if recieved value is correct', a
       expect(typeof match).toBe('object') // checking if received valid object
     }))
   }))
-})
+});
+
+// testing if our dateConstructor working properly
 
 it('construct date object from string in dd/mm/yy and HH:MM format', () =>{
  const dateString = '25/12/17';
@@ -44,8 +46,23 @@ it('construct date object from string in dd/mm/yy and HH:MM format', () =>{
  let testString = test.toString()
  let matchString = 'Dec 25 2017 15:00:00'
  expect(testString.includes(matchString)).toBe(true);
- 
-}) 
+}); 
 
+
+// testing the sorting Algorithm
+
+it( ' testing if sorting algorithm works,', async () =>{
+const result = await fetchTournaments()// fetching matches from tournaments
+.then((res) => res.map((tournament) =>{
+  return tournament._tid
+}))
+.then((res) => res.map((tournamentID) =>{
+    fetchMatches(tournamentID)
+    .then((res) =>{
+    const test = sortMatches(res); // start testing sorting algorithm
+    expect(test.length <= 5).toBe(true);// check if we returned last 5 matches
+    })
+  }))
+})
 
  
